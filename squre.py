819f033e42e40cdc6bb066be4efd3550955ef96e
mraviel <mraviel@outlook.com>
2019-05-18 10:44:10 +0300

    Add the squre class

---
+++ squre.py
@@ -0,0 +1,9 @@
+
+
+class Squre():
+
+    def __init__(self, color, pos, isTaken):
+
+        self.color = color  # the color of the squre (black or white)
+        self.pos = pos
+        self.isTaken = isTaken  # is there any piece on the squre?
