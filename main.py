1b1688c65ac257ec6777416b32c8ffcc69d29136
mraviel <mraviel@outlook.com>
2019-05-18 10:20:49 +0300

    Initialize the game settings and the main file.

---
+++ main.py
@@ -0,0 +1,82 @@
+import pygame as pg
+from setting import *
+from os import path
+
+# location of the img folder
+img_dir = path.join(path.dirname(__file__), 'images')
+sound_dir = path.join(path.dirname(__file__), 'sound')
+
+class Game:
+
+    def __init__(self):
+        # initiallize game window, etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # start a new game
+        self.run()
+
+    def run(self):
+        # Game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game loop - update
+        pass
+
+
+    def events(self):
+        # Game loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                self.mx, self.my = pg.mouse.get_pos()
+                print (self.mx, self.my)
+
+    def draw(self):
+        # Game loop - draw
+        self.screen.fill((43,123,21))
+
+        # after drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # the start screen
+        pass
+
+    def show_go_screen(self):
+        # the game-over screen
+        pass
+
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+
+
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ setting.py
@@ -0,0 +1,16 @@
+# Game settings
+
+TITLE = "             Checkers       "
+WIDTH = 920
+HEIGHT = 920
+FPS = 12
+FONT_NAME = 'arial'
+
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
